d0b3dd964211798f2872dd4df417607cf7b32372
Jhefferson <jhefferson.santos@marata.com.br>
2026-01-29 12:44:04 -0300

    hotfix: import log

--- backend/app/Backend/Http/Controllers/Auth/OAuthController.php
+++ backend/app/Backend/Http/Controllers/Auth/OAuthController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Log;
 
 class OAuthController extends Controller
 {
